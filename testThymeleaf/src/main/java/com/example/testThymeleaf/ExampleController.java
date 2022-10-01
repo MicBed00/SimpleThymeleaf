87078579f8e7c87aa45c79debcb8073ef945c719
Michał Będziak <michal.bedziak@gmail.com>
2022-10-01 21:02:01 +0200

    stowrzenie obiketu Car i wyświetlenie info

--- testThymeleaf/src/main/java/com/example/testThymeleaf/ExampleController.java
+++ testThymeleaf/src/main/java/com/example/testThymeleaf/ExampleController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ExampleController {
 
-    @GetMapping
+    @GetMapping("/")
     String getExp(Model model) {
-
+        Car car1 = new Car("BMW", "i8");
         model.addAttribute("some", "This is coming from Controller");
         model.addAttribute("var1", "Hello World!");
+        model.addAttribute("car", car1);
         return "exp";
     }
 }
